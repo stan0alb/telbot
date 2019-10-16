3f3b244b204f50dd6f4c175cda56cfaeb6286660
stan0alb <38112809+stan0alb@users.noreply.github.com>
2019-10-17 02:33:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,11 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+import config
+import telebot
 
-from config import TOKEN
+bot = telebot.TeleBot(config.token)
 
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
-
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
-
-
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
-
-
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
-
-
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+    
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    bot.polling(none_stop=True)
