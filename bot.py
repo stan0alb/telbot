3df899b54b1e3ff3f1300a2c535e47b10c3055aa
stan0alb <38112809+stan0alb@users.noreply.github.com>
2019-10-17 04:41:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
-import config
 import telebot
 
-bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
-    
+@bot.message_handler(commands=['test'])
+def find_file_ids(message):
+    for file in os.listdir('music/'):
+        if file.split('.')[-1] == 'ogg':
+            f = open('music/'+file, 'rb')
+            msg = bot.send_voice(message.chat.id, f, None)
+            # А теперь отправим вслед за файлом его file_id
+            bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id=msg.message_id)
+        time.sleep(3)
+
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
